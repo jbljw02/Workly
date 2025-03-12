00b9f783a895c029290d62136a7e1d330a0fd1e0
Jinu <jbljw02@naver.com>
2025-03-12 22:32:40 +0900

    refactor: 동적 페이지 초기 메타데이터 즉시 생성

--- src/app/demo/[folderId]/[id]/page.tsx
+++ src/app/demo/[folderId]/[id]/page.tsx
@@ -2,9 +2,14 @@ import DemoEditor from "@/components/demo/DemoEditor";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { Metadata } from "next";
+import { getDocumentMetadata } from "@/utils/document/getDocumentMetadata";
 
-export const metadata: Metadata = {
-    title: 'Workly',
+export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
+    const metadata = await getDocumentMetadata(params.id);
+    if (!metadata) {
+        return redirect('/document-not-found');
+    }
+    return metadata;
 }
 
 export default function DemoPage({ params }: { params: { id: string } }) {
